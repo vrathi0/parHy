e7127a43218ab508d45e78765cafd44e845390c2
vrathi0 <48102204+vrathi0@users.noreply.github.com>
2021-02-05 22:28:10 -0600

    Update hysplit_trajectory.R

--- R/hysplit_trajectory.R
+++ R/hysplit_trajectory.R
@@ -69,7 +69,7 @@
 #'   )
 #' }
 #' 
-#' @export
+#' @export 
 hysplit_trajectory <- function(lat = 49.263,
                                lon = -123.250,
                                height = 50,
@@ -87,8 +87,13 @@ hysplit_trajectory <- function(lat = 49.263,
                                binary_path = NULL,
                                met_dir = NULL,
                                exec_dir = NULL,
-                               softrun = NULL, # This is not evaluated, yet
-                               clean_up = TRUE) {
+                               clean_up = TRUE,
+                               name_source = NULL,
+                               id_source = NULL,
+                               db = TRUE,
+                               cred = NULL,
+                               table_name = 'trajectories_hysplit',
+                               schema = 'hysplit') {
   
   # If the execution dir isn't specified, use the working directory
   if (is.null(exec_dir)) exec_dir <- getwd()
@@ -102,13 +107,16 @@ hysplit_trajectory <- function(lat = 49.263,
       binary_path = binary_path,
       binary_name = "hyts_std"
     )
-  
+
   # Get the system type
   system_type <- get_os()
   
   # Generate name of output folder
   if (is.null(traj_name)) {
-    folder_name <- paste0("traj-", format(Sys.time(), "%Y-%m-%d-%H-%M-%S"))
+    folder_name <- paste0("traj-", 
+                          as.character(Sys.getpid()), "-",
+                          sample(1:1e10, 1), "-",
+                          format(Sys.time(), "%Y-%m-%d-%H-%M-%S"))
   } else if (!is.null(traj_name)) {
     folder_name <- traj_name
   }
@@ -142,11 +150,7 @@ hysplit_trajectory <- function(lat = 49.263,
     config_list[tm_names] <- 1
   }
   
-  # Write the config and ascdata lists to files in
-  # the `exec` directory
-  config_list %>% write_config_list(dir = exec_dir)
-  ascdata_list %>% write_ascdata_list(dir = exec_dir)
-  
+ 
   # Stop function if there are vectors of different
   # length for `lat` and `lon`
   if (length(lat) != length(lon)) {
@@ -166,8 +170,11 @@ hysplit_trajectory <- function(lat = 49.263,
   
   # Generate a tibble of receptor sites
   receptors_tbl <- 
-    dplyr::tibble(lat = lat, lon = lon) %>%
-    dplyr::group_by(lat, lon) %>% 
+    dplyr::tibble(lat = lat, 
+                  lon = lon,
+                  id_source = id_source,
+                  name_source = name_source) %>%
+    dplyr::group_by(lat, lon, id_source, name_source) %>% 
     tidyr::expand(height = height) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(receptor = dplyr::row_number()) %>%
@@ -225,6 +232,27 @@ hysplit_trajectory <- function(lat = 49.263,
         } else {
           full_year_GMT <- paste0("20", start_year_GMT)
         }
+
+        # Create model folder name for parallel execution
+        model_folder_name <- paste0("model-", 
+                                    as.character(Sys.getpid()), '-', 
+                                    format(Sys.time(), "%Y-%m-%d-%H-%M-%S"), '-',
+                                    start_year_GMT, '-',
+                                    start_month_GMT, '-',
+                                    start_day_GMT, '-',
+                                    start_hour_GMT)
+
+        model_folder_path  <- file.path(exec_dir, model_folder_name)
+
+
+        if (!dir.exists(model_folder_path)){
+            dir.create(model_folder_path)
+        }
+        
+        # Write the config and ascdata lists to files in
+        # the `exec` directory
+        config_list %>% write_config_list(dir = model_folder_path)
+        ascdata_list %>% write_ascdata_list(dir = model_folder_path)
         
         # Construct the output filename string for this
         # model run
@@ -262,7 +290,7 @@ hysplit_trajectory <- function(lat = 49.263,
           output_filename = output_filename,
           system_type = system_type,
           met_dir = met_dir,
-          exec_dir = exec_dir
+          exec_dir = model_folder_path
         )
         
         # The CONTROL file is now complete and in the
@@ -270,41 +298,40 @@ hysplit_trajectory <- function(lat = 49.263,
         sys_cmd <- 
           paste0(
             "(cd \"",
-            exec_dir,
+            model_folder_path,
             "\" && \"",
             binary_path,
             "\" ",
-            to_null_dev(system_type = system_type),
+            to_log_dev(system_type = system_type),
             ")"
           )
-        
         execute_on_system(sys_cmd, system_type = system_type)
+
+        recep_file_path <- file.path(exec_dir, id_source, folder_name)
+    
+        recep_file_path_stack <- 
+          c(recep_file_path_stack, file.path(exec_dir, id_source))
         
+        # Create the output folder if it doesn't exist
+        if (!dir.exists(recep_file_path)) {
+          dir.create(path = recep_file_path, recursive = TRUE)
+        }
+        
+        # Move files into the output folder
+        file.copy(
+          from = file.path(model_folder_path, trajectory_files),
+          to = recep_file_path,
+          copy.mode = TRUE
+        )
+        
+        unlink(file.path(model_folder_path, trajectory_files), force = TRUE)
+
       }
     }
     
-    recep_file_path <- file.path(exec_dir, receptor_i, folder_name)
-    
-    recep_file_path_stack <- 
-      c(recep_file_path_stack, file.path(exec_dir, receptor_i))
-    
-    # Create the output folder if it doesn't exist
-    if (!dir.exists(recep_file_path)) {
-      dir.create(path = recep_file_path, recursive = TRUE)
-    }
-    
-    # Move files into the output folder
-    file.copy(
-      from = file.path(exec_dir, trajectory_files),
-      to = recep_file_path,
-      copy.mode = TRUE
-    )
-    
-   # unlink(file.path(exec_dir, trajectory_files), force = TRUE)
-    
     # Obtain a trajectory data frame
     traj_tbl <-
-      trajectory_read(output_folder = recep_file_path) %>%
+      dirtywind::trajectory_read(output_folder = recep_file_path) %>%
       dplyr::as_tibble() %>%
       dplyr::mutate(
         receptor = receptor_i,
@@ -319,8 +346,8 @@ hysplit_trajectory <- function(lat = 49.263,
   }
 
   if (clean_up) {
-   # unlink(file.path(exec_dir, traj_output_files()), force = TRUE)
-   # unlink(recep_file_path_stack, recursive = TRUE, force = TRUE)
+    unlink(file.path(exec_dir, traj_output_files()), force = TRUE)
+    unlink(recep_file_path_stack, recursive = TRUE, force = TRUE)
   }
   
   ensemble_tbl <-
@@ -357,7 +384,7 @@ hysplit_trajectory <- function(lat = 49.263,
       dplyr::arrange(receptor, traj_dt_i, dplyr::desc(hour_along))
   }
   
-  ensemble_tbl %>%
+  ensemble_tbl_complete  <- ensemble_tbl %>%
     dplyr::right_join(
       ensemble_tbl %>%
         dplyr::select(receptor, traj_dt_i, lat_i, lon_i, height_i) %>%
@@ -365,5 +392,31 @@ hysplit_trajectory <- function(lat = 49.263,
         dplyr::mutate(run = dplyr::row_number()),
       by = c("receptor", "traj_dt_i", "lat_i", "lon_i", "height_i")
     ) %>%
-    dplyr::select(run, dplyr::everything())
+    dplyr::select(run, dplyr::everything()) 
+   
+  if (isTRUE(db)) {
+
+      con <- dirtywind::mydb(cred)
+
+      table_hysplit_sql_id = DBI::Id(schema = schema,
+                                  table = table_name)
+
+      ens_table <- ensemble_tbl_complete  %>%
+          mutate(name_source = name_source,
+                 id_source = id_source) %>%
+          dplyr::select(name_source, id_source, dplyr::everything()) 
+          
+      DBI::dbWriteTable(conn = con, 
+                        name = table_hysplit_sql_id,
+                        value = ens_table,
+                        append = TRUE,
+                        overwrite = FALSE)
+
+  } else {
+    ensemble_tbl_complete  %>%
+          mutate(name_source = name_source,
+                 id_source = id_source) %>%
+          dplyr::select(name_source, id_source, dplyr::everything())
+  }
+
 }
